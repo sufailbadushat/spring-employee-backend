90d931db0c4fbf424583c57ab3d1bf9b3ceb3a41
sufailbadushat <sufailbadushat@gamil.com>
2022-12-05 17:29:28 +0530

    post method add

--- src/main/java/com/nest/employee_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employee_backend/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.nest.employee_backend.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,21 +14,21 @@ public class EmployeeController {
         return "Welcome to Main page!";
     }
 
-    @GetMapping("add")
+    @PostMapping("add")
     public  String AddEmployee(){ return "Welcome to add employee page!"; }
 
-    @GetMapping("search")
+    @PostMapping("search")
     public String SearchEmployee(){ return "Welcome to Search employee page!"; }
 
     @GetMapping("viewAll")
     public String ViewAllEmployee(){ return "Welcome to View all employee page!"; }
 
-    @GetMapping("edit")
+    @PostMapping("edit")
     public String EditEmployee() {
             return "Welcome to Edit employee page!";
     }
 
-    @GetMapping("delete")
+    @PostMapping("delete")
     public String DeleteEmployee(){
         return "Welcome to Delete employee page!";
 
