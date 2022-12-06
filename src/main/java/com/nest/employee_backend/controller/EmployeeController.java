a1d1c4ad12fafbeeaceadd440419aff90852c1e3
sufailbadushat <sufailbadushat@gamil.com>
2022-12-06 15:21:45 +0530

    add data to the database

--- src/main/java/com/nest/employee_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employee_backend/controller/EmployeeController.java
@@ -1,21 +1,25 @@
 package com.nest.employee_backend.controller;
 
 
+import com.nest.employee_backend.dao.EmployeeDao;
 import com.nest.employee_backend.model.Employees;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
 public class EmployeeController {
+
+    @Autowired
+    EmployeeDao employeeDao;
 
     @GetMapping("/")
     public String MainPage(){
         return "Welcome to Main page!";
     }
 
+
+    @CrossOrigin(origins = "*")
     @PostMapping(path = "add", consumes = "application/json", produces = "application/json")
     public  String AddEmployee(@RequestBody Employees employees) {
 
@@ -26,6 +30,9 @@ public class EmployeeController {
         System.out.println(employees.getMobileNumber().toString());
         System.out.println(employees.getUsername().toString());
         System.out.println(employees.getPassword().toString());
+
+        employeeDao.save(employees);
+
         return "Employee added successfully!";
     }
 
